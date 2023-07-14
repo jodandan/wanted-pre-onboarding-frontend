d570b1f2641689a4c8957666e9c4f792fff2ec4f
jodan <wdd789q@naver.com>
2023-07-14 13:36:30 +0900

    feat : wanted task end

--- src/App.js
+++ src/App.js
@@ -1,42 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Signin from './components/Signin';
 import Signup from './components/Signup';
 import Todo from './components/Todo';
 
 function App() {
-  const [token, setToken] = useState(localStorage.getItem('token'));
-
-  useEffect(() => {
-    const storedToken = localStorage.getItem('token');
-    setToken(storedToken);
-  }, []);
-
-  const handleLogin = (newToken) => {
-    localStorage.setItem('token', newToken);
-    setToken(newToken);
-  };
-
-  const handleLogout = () => {
-    localStorage.removeItem('token');
-    setToken(null);
-  };
-
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Signup />} />
-        <Route
-          path="/signin"
-          element={<Signin onLogin={handleLogin} />}
-        />
-        <Route
-          path="/todo"
-          element={
-            token ? <Todo onLogout={handleLogout} /> : <Navigate to="/signin" />
-          }
-        />
-      </Routes>
+      <Switch>
+        <Route path="/" exact>
+          <Signup />
+        </Route>
+        <Route path="/signin">
+          <Signin />
+        </Route>
+        <Route path="/todo">
+          <Todo />
+        </Route>
+      </Switch>
     </Router>
   );
 }
